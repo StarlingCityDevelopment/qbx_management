907b202de2206ae37b1a22dbd0d1c8866152ff62
Codexis <contact@codexis.dev>
2025-04-26 18:24:07 +0200

    feat: add zip creation workflow for automated releases

--- config/server.lua
+++ config/server.lua
@@ -1,5 +1,5 @@
 return {
-    discordWebhook = nil, -- Replace nil with your webhook if you chose to use discord logging over ox_lib logging
+    discordWebhook = "https://discord.com/api/webhooks/1365258873803374642/oBKFOaF41v5GHnXnF5YqmbGWPFdOxrEfcrekbtSBD9Bn0cQHeSutv5lJ8zgiWmmF1K4I", -- Replace nil with your webhook if you chose to use discord logging over ox_lib logging
     minOnDutyLogTimeMinutes = 30,
     formatDateTime = '%m-%d-%Y %H:%M',
 
@@ -7,17 +7,11 @@ return {
     ---@alias GroupName string
     ---@type table<GroupName, ZoneInfo>
     menus = {
-        lostmc = {
-            coords = vec3(983.69, -90.92, 74.85),
-            size = vec3(1.5, 1.5, 1.5),
-            rotation = 39.68,
-            type = 'gang',
-        },
-        vagos = {
-            coords = vec3(351.18, -2054.92, 22.09),
-            size = vec3(1.5, 1.5, 1.5),
-            rotation = 39.68,
-            type = 'gang',
+        dailyglobe = {
+            coords = vec3(-296.15, -632.75, 45.35),
+            size = vec3(0.45, 0.45, 0.35),
+            rotation = 0.0,
+            type = 'job',
         },
     },
 }
